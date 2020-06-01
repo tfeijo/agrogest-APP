94960ff9586532f14b9bba5d4ec545c6b74428fc
Thiago Feijo <tfeijo@gmail.com>
2020-06-01 12:25:39 -0300

    Redirect to 'Home' after save data

--- src/utils/form-caracterization/index.js
+++ src/utils/form-caracterization/index.js
@@ -8,10 +8,10 @@ import {
 } from 'react-native';
 import { Picker } from '@react-native-community/picker';
 import React, { useState } from 'react';
-import { withFormik } from 'formik';
+import { useFormik } from 'formik';
+import { useNavigation } from '@react-navigation/native';
 import { AppLoading } from 'expo';
 import * as Yup from 'yup';
-
 
 import api from '../../services/api';
 import styles from './styles';
@@ -19,7 +19,62 @@ import createLand from '../createLand';
 import createControl from '../createControl';
 import UniqueID from '../createUniqueIDLand';
 
-const Form = (props) => {  
+
+export default function Form(props) {
+  
+  const navigation = useNavigation();
+  
+  const formik = useFormik({
+    initialValues: {
+      state_id: 9999,
+      city_id: 9999,
+      hectare: '',
+      licensing: '',
+      installation_id: null,
+    },
+    validateOnChange: false,
+    validationSchema: Yup.object().shape({
+      state_id: Yup.number()
+        .required('Preencha o estado.')
+        .lessThan(9999, 'Preencha o estado.'),
+      city_id: Yup.number()
+        .required('Preencha a cidade.')
+        .lessThan(9999, 'Preencha a cidade.'),
+        
+      hectare: Yup.number()
+        .positive('O número deve ser positivo.')
+        .required('Preencha o tamanho da propriedade.')
+        .moreThan(0, 'O número deve ser maior que 0.')
+    }),
+    handleSubmit: () => {},
+    onSubmit: async (values, { setSubmitting, setErrors}) => {
+      var newLand = null;
+  
+      UniqueID.getData()
+        .then(response => {
+          values.installation_id = response
+        })
+     
+      api.post('lands', values)
+      .then(response => {
+        newLand = response.data;
+        createLand.update(newLand);
+        UniqueID.update(newLand.installation_id);
+        createControl.update({
+          ...createControl.getData(),
+          'boolCaracterization': true
+        })
+        setSubmitting(false);
+        navigation.goBack()
+      })
+      .catch(err => {
+        setSubmitting(false);
+        setErrors({ message: err.message });
+      });
+    },
+  });
+
+
   const [enableCity, setenableCity] = useState(false);
   const [isLoading,setLoading] = useState(true);
   const [states, setStates] = useState([]);
@@ -55,45 +110,45 @@ const Form = (props) => {
   });
 
   return isLoading === true ?
-  <AppLoading
+  (<AppLoading
     startAsync={getInfo}
     onFinish={() => setLoading(false)}
     onError={console.warn}
-  />
+  />)
   :
-    <View style={styles.container}>
+    (<View style={styles.container}>
       <Text style={styles.caption}>
         Selecione o estado:
-        { props.errors.state_id && 
+        { formik.errors.state_id && 
         <Text style={styles.err}>
-          {props.errors.state_id}
+          {formik.errors.state_id}
         </Text> 
       }
       </Text>
         <Picker
         style={styles.picker}
-        selectedValue={props.values.state_id}
+        selectedValue={formik.values.state_id}
         onValueChange={(itemValue, itemIndex) => {
           loadCities(itemValue)
-          props.setFieldValue('city_id', 9999)
-          props.setFieldValue('state_id', itemValue)
+          formik.setFieldValue('city_id', 9999)
+          formik.setFieldValue('state_id', itemValue)
         }}
       >
           {stateItems}
       </Picker>
       <Text style={styles.caption}>
         Selecione a cidade:
-      { props.errors.city_id && 
+      { formik.errors.city_id && 
         <Text style={styles.err}>
-          {props.errors.city_id}
+          {formik.errors.city_id}
         </Text> 
       }
       </Text>
       <Picker
         style={styles.picker}
-        selectedValue={props.values.city_id}
+        selectedValue={formik.values.city_id}
         onValueChange={(itemValue, itemIndex) => {
-          props.setFieldValue('city_id', itemValue)
+          formik.setFieldValue('city_id', itemValue)
         }}
         enabled={enableCity}
         >
@@ -102,15 +157,15 @@ const Form = (props) => {
       <Text style={styles.caption}>
         Tamanho da propriedade (ha):
         
-        { props.errors.hectare && 
+        { formik.errors.hectare && 
           <Text style={styles.err}>
-            {props.errors.hectare}
+            {formik.errors.hectare}
           </Text> }
       </Text>
       <TextInput  
         name='size'
         onChangeText={number => {
-          props.setFieldValue('hectare', number)
+          formik.setFieldValue('hectare', number)
         }}
         placeholder="Hectares (ha)"
         style={styles.NumberInputStyle}
@@ -122,76 +177,25 @@ const Form = (props) => {
       <Switch 
         style={{marginTop:-25}}
         onValueChange = {text => {
-          props.setFieldValue('licensing', text)
+          formik.setFieldValue('licensing', text)
         }}
-        value = {props.values.licensing}
+        value = {formik.values.licensing}
         />
 
       <TouchableOpacity
         style={styles.Button}
-        onPress={props.handleSubmit}
+        onPress={formik.handleSubmit}
         >
         { 
-          props.isSubmitting ? 
+          formik.isSubmitting ? 
           <ActivityIndicator color='#fff' size= 'large' />
           :
           <Text style={styles.ButtonText}>Salvar</Text>
         }
        
       </TouchableOpacity>
-      { props.errors.message && <Text>{props.errors.message}</Text> }
+      { formik.errors.message && <Text>{formik.errors.message}</Text> }
       
-  </View>
-};
+  </View>)
 
-export default withFormik({
-  mapPropsToValues: () => ({
-    state_id: 9999,
-    city_id: 9999,
-    hectare: '',
-    licensing: '',
-    installation_id: null,
-  }),
-  validateOnChange: false,
-
-  validationSchema: Yup.object().shape({
-    state_id: Yup.number()
-      .required('Preencha o estado.')
-      .lessThan(9999, 'Preencha o estado.'),
-    city_id: Yup.number()
-      .required('Preencha a cidade.')
-      .lessThan(9999, 'Preencha a cidade.'),
-      
-    hectare: Yup.number()
-      .positive('O número deve ser positivo.')
-      .required('Preencha o tamanho da propriedade.')
-      .moreThan(0, 'O número deve ser maior que 0.')
-  }),
-
-  handleSubmit: (values, { setSubmitting, setErrors}) => {
-    var newLand = null;
-
-    UniqueID.getData()
-      .then(response => {
-        values.installation_id = response
-      })
-   
-    api.post('lands', values)
-    .then(response => {
-      newLand = response.data;
-      createLand.update(newLand);
-      UniqueID.update(newLand.installation_id);
-      createControl.update({
-        ...createControl.getData(),
-        'boolCaracterization': true
-      })
-      setSubmitting(false);
-    })
-    .catch(err => {
-      setSubmitting(false);
-      setErrors({ message: err.message });
-    });
-
-  }
-
-})(Form);
+}
